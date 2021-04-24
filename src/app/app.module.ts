4c631f513ac935b7135706cf78ba272d7cccbe91
Mehdi <elwafi.courrier99@gmail.com>
2021-04-24 15:29:51 +0000

    Added a admin,collaborateur,chef-equipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChefEquipeComponent } from './chef-equipe/chef-equipe.component';
+import { CollaborateurComponent } from './collaborateur/collaborateur.component';
+import { AdministrateurComponent } from './administrateur/administrateur.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ChefEquipeComponent,
+    CollaborateurComponent,
+    AdministrateurComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chef-equipe/chef-equipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chef-equipe',
+  templateUrl: './chef-equipe.component.html',
+  styleUrls: ['./chef-equipe.component.css']
+})
+export class ChefEquipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
