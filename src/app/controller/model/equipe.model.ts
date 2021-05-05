4dc52fde55bd3799a0de5cfcb738d397940308d6
AHMAMAAMINE <amineahmama17@gmail.com>
2021-05-05 21:45:12 +0000

    StockInterface

--- src/app/controller/model/equipe.model.ts
+++ src/app/controller/model/equipe.model.ts
@@ -3,6 +3,6 @@ export class Equipe {
  public id : number;
  public libelle : string;
  public ref: string ;
- public membres : Array<MembreEquipe>;
- public chefEquipe : MembreEquipe;
+ public membres =new  Array<MembreEquipe>();
+ public chefEquipe =new MembreEquipe();
 }
